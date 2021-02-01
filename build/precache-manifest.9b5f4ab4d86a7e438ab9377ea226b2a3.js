bb9a9b2fad6a5ee57f9b200b121a3d44b9548329
oliviawb <68314116+oliviawb@users.noreply.github.com>
2021-02-01 13:51:31 -0500

    first commit

---
+++ build/precache-manifest.9b5f4ab4d86a7e438ab9377ea226b2a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298ab769874928806154a510ee84c017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bdf25b46851203496c0",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "4b1e88568c74c8827723",
+    "url": "/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "1bdf25b46851203496c0",
+    "url": "/static/js/2.816c6044.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.816c6044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1e88568c74c8827723",
+    "url": "/static/js/main.1b3a5e1d.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
